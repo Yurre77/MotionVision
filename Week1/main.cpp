b983fa71b94e334d0ef505a566b7efb4388e69d5
Yurre <yurrr.schram@gmail.com>
2024-09-04 14:00:14 +0200

    start defining functions

--- Week1/main.cpp
+++ Week1/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
+
+class Segment {
+    public:
+        int angle;
+        int length;
+};
 
 int main(){
     cout << "Hello world";
     return 0;
 }
 
+int calculateEnd(){
+
+}
+
+float calculateSin(){
+
+}
+
+float calculateCos(){
+    
+}
