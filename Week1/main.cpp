718fb466a525cfedf89451168c9040c6757ebdd3
Yurre <yurrr.schram@gmail.com>
2024-09-06 11:10:43 +0200

    added a position class

--- Week1/main.cpp
+++ Week1/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <cmath>
+#include <array>
 #include <vector>
 
 using namespace std;
 
+class Position {
+    public:
+        double x;
+        double y;
+};
+
+int main(){
+
+    return 0;
+}
