78f10d3e154e84c8d3744b486b69f01dfab139e7
Yurre <yurrr.schram@gmail.com>
2024-09-06 11:00:49 +0200

    start from scratch again

--- Week1/main.cpp
+++ Week1/main.cpp
@@ -4,36 +4,3 @@
 
 using namespace std;
 
-#define JOINTS 3
-#define MAX_TRIES 100
-
-//make a class to keep track of positions
-class Position{
-    public:
-        int x;
-        int y;
-};
-
-//make a class to keep track of each segment
-class Segment{
-    public:
-        int length;
-        int angle;
-        Position position;
-};
-
-int makeArm(Segment points[]){
-    for(int i = 0; i < points->length; i++){
-        points[i].position.x = 0;
-        points[i].position.y = (0 + i);
-    }
-    return 0;
-}
-
-int main(){
-    Segment points[JOINTS];
-
-    makeArm(points);
-
-    return 0;
-}
