b6ab1e3ed21a8cdae5cc0687d49b4a6777034d06
Yurre <yurrr.schram@gmail.com>
2024-09-06 12:53:20 +0200

    just a couple warnings left now

--- Week1/main.cpp
+++ Week1/main.cpp
@@ -10,26 +10,20 @@ void fk(const vector<double>& q, double (*p)[4]){
     const double L1=1.0, L2=1.0, L3 = 1.0;
 
     //position vectors of each joint
-    p[0][0] = 0.0;
-    p[0][1] = 0.0;
-    p[0][2] = 0.0;
+    *p[0] = 0.0, 0.0, 0.0;
 
     //calculations for forward kinematics
-
     //p1: pose of joint #2 (end of link 1)
-    p[1][0] = p[0][0] + L1 * cos(q[0]);
-    p[1][1] = p[0][1] + L1 * sin(q[0]);
-    p[1][2] = q[0];
+    *p[1] = L1 * cos(q[0]), L1 * sin(q[0]), q[0];
+    *p[1] = *p[0] + *p[1];
 
     //p2: pose of joint #3 (end of link 2)
-    p[2][0] = p[1][0] + L2 * cos(q[0] + q[1]);
-    p[2][1] = p[1][1] + L2 * sin(q[0] + q[1]);
-    p[2][2] = q[1];
+    *p[2] = L2 * cos(q[0] + q[1]), L2 * sin(q[0] + q[1]), q[1];
+    *p[2] = *p[1] + *p[2];
 
     //p3: pose of the tool end (end of link 3)
-    p[3][0] = p[2][0] + L3 * cos(q[0] + q[1] + q[2]);
-    p[3][1] = p[2][1] + L3 * sin(q[0] + q[1] + q[2]);
-    p[3][2] = p[2][2] + L3 * sin(q[1] + q[2]);
+    *p[3] = L3 * cos(q[0] + q[1] + q[2]), L3 * sin(q[0] + q[1] + q[2]), q[2];
+    *p[3] = *p[2] + *p[3];
 }
 
 int main(){
@@ -48,7 +42,7 @@ int main(){
 
     //output the pose matrix
     for(const auto& pose : p){
-        cout << "(" << pose[0] << "," << pose[1] << pose[2] << ")" << endl;
+        cout << "(" << pose[0] << ", " << pose[1] << ", " << pose[2] << ")" << endl;
     }
 
     return 0;
