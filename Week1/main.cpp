51a81365b8ddc3c81ac7ce373446fd702e49c458
Yurre <yurrr.schram@gmail.com>
2024-09-06 12:38:11 +0200

    changed the code slightly

--- Week1/main.cpp
+++ Week1/main.cpp
@@ -9,30 +9,27 @@ void fk(const vector<double>& q, double (*p)[4]){
     //Arm links
     const double L1=1.0, L2=1.0, L3 = 1.0;
 
-    //
-
     //position vectors of each joint
-    vector <double> p0 = {0.0, 0.0, 0.0};
-    vector <double> p1;
-    vector <double> p2;
-    vector <double> p3;
+    p[0][0] = 0.0;
+    p[0][1] = 0.0;
+    p[0][2] = 0.0;
 
     //calculations for forward kinematics
 
     //p1: pose of joint #2 (end of link 1)
-    p1[0] = p0[0] + L1 * cos(q[0]);
-    p1[1] = p0[1] + L1 * sin(q[0]);
-    p1[2] = q[0];
+    p[1][0] = p[0][0] + L1 * cos(q[0]);
+    p[1][1] = p[0][1] + L1 * sin(q[0]);
+    p[1][2] = q[0];
 
     //p2: pose of joint #3 (end of link 2)
-    p2[0] = p1[0] + L2 * cos(q[0] + q[1]);
-    p2[1] = p1[1] + L2 * sin(q[0] + q[1]);
-    p2[2] = q[1];
+    p[2][0] = p[1][0] + L2 * cos(q[0] + q[1]);
+    p[2][1] = p[1][1] + L2 * sin(q[0] + q[1]);
+    p[2][2] = q[1];
 
     //p3: pose of the tool end (end of link 3)
-    p3[0] = p2[0] + L3 * cos(q[0] + q[1] + q[2]);
-    p3[1] = p2[1] + L3 * sin(q[0] + q[1] + q[2]);
-    p3[2] = p2[2] + L3 * sin(q[1] + q[2]);
+    p[3][0] = p[2][0] + L3 * cos(q[0] + q[1] + q[2]);
+    p[3][1] = p[2][1] + L3 * sin(q[0] + q[1] + q[2]);
+    p[3][2] = p[2][2] + L3 * sin(q[1] + q[2]);
 }
 
 int main(){
@@ -46,6 +43,7 @@ int main(){
     double (*ptr)[4];
     ptr = p;
 
+    //do the fk calculations
     fk(q, ptr);
 
     return 0;
