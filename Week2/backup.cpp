90a5663c03664436b46779a4d3146dca21e9ae96
Yurre <yurrr.schram@gmail.com>
2024-09-24 16:36:51 +0200

    setup

--- Week2/backup.cpp
+++ Week2/backup.cpp
@@ -1,3 +1,12 @@
+#include <cmath>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
 class robotArm{
-    
+    public:
+        double L1;
+        double L2;
+        double L3;
 };
