513b30abff70906c5e1f34f7e278aad162dd1542
Yurre <yurrr.schram@gmail.com>
2024-09-24 16:44:33 +0200

    forward kinematics done

--- Week2/backup.cpp
+++ Week2/backup.cpp
@@ -10,10 +10,13 @@ class robotArm{
         double L2;
         double L3;
 
-    double fk(){
+    double fk(double theta1, double theta2, double theta3){
 
         double x;
         double y;
+
+        x = (L1 * cos(theta1) + L2 * cos(theta1 + theta2) + L3 * cos(theta1 + theta2 + theta3));
+        y = (L1 * sin(theta1) + L2 * sin(theta1 + theta2) + L3 * sin(theta1 + theta2 + theta3));
 
         return (x, y);
     }
