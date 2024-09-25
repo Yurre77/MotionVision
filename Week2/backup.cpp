0a05e6fcacff76fece4596dca8b0b030dadb90bf
Yurre <yurrr.schram@gmail.com>
2024-09-25 12:13:27 +0200

    added dot product and crossproduct

--- Week2/backup.cpp
+++ Week2/backup.cpp
@@ -25,3 +25,29 @@ class robotArm{
         
     }
 };
+
+//function to determine dot product
+double dotProduct(vector<double> V1, vector<double> V2){
+    double result;
+
+    double Ax = V1[0] * V2[0];
+    double Ay = V1[1] * V2[1];
+    double Az = V1[2] * V2[2];
+
+    result = Ax + Ay + Az;
+
+    return result;
+}
+
+//function to calculate cross product
+vector<double> crossProduct(vector<double> v1, vector<double> v2){
+
+    vector<double> returnV = {0.0, 0.0, 0.0};
+
+    //calculate the return vector
+    returnV[0] = v1[1] * v2[2] - v1[2] * v2[1];
+    returnV[1] = v1[2] * v2[0] - v1[0] * v2[2];
+    returnV[2] = v1[0] * v2[1] - v1[1] * v2[0];
+
+    return returnV;
+}
